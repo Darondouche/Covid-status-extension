d8e509d388d87cb8e0b824774df46ab9b1f16f67
Alice <darondeau.alice@gmail.com>
2023-01-15 20:29:34 +0100

    données plus pertinentes choisies dans l'affichage

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ async function fetchData(){
     const record = await res.json();
 
     document.getElementById("date").innerHTML = record.data.update_date_time;
-    document.getElementById("cases").innerHTML = record.data.local_new_cases;
-    document.getElementById("latestBy").innerHTML = record.data.local_deaths;
-    document.getElementById("geathNew").innerHTML = record.data.local_recovered;
+    document.getElementById("today_cases").innerHTML = record.data.local_new_cases;
+    document.getElementById("total_deaths_LK").innerHTML = record.data.local_deaths;
+    document.getElementById("total_deaths").innerHTML = record.data.global_deaths;
 
 }
 
